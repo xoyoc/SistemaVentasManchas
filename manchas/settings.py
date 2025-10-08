f5801f92259f63a17fb05293d43afb31c353a74e
xoyoc <xoyoc_l2@hotmail.com>
2025-10-07 18:01:28 -0600

    Correcion de produccion

--- manchas/settings.py
+++ manchas/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7cl)b28*)!doz%2jl+kv+zqq^sozhvhs!jl#cz2qs$i!8oqcv'
+SECRET_KEY = config('SECRET_KEY', default='tu-clave-secreta-aqui')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
